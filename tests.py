79e50693321116d3e669df70d6bdc6c55f857d76
Denise Salazar <denise.salazar.1210@gmail.com>
2018-10-10 14:31:32 -0700

    Update tests.py to test existing Flask routes

--- tests.py
+++ tests.py
@@ -38,78 +38,103 @@ class TestRoutes(unittest.TestCase):
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'<h1>Log In</h1>', result.data)
 
-# class TestSignUp(unittest.TestCase):
+class TestRoutesWithDatabase(unittest.TestCase):
 
-#     def setUp(self):
-#         """ Set up before every test. """
+    def setUp(self):
+        """ Set up before every test. """
 
-#         self.client = app.test_client()
-#         app.config['TESTING'] = True
+        self.client = app.test_client()
+        app.config['TESTING'] = True
 
-#         # connect to test database
-#         connect_to_db(app, 'postgresql:///testdb')
+        # connect to test database
+        connect_to_db(app, 'postgresql:///testdb')
 
-#         # create tables and add sample data
-#         db.create_all()
-#         load_dummy_data()
+        # create tables and add sample data
+        db.create_all()
+        load_dummy_data()
 
-#         # # go to signup page
-#         # self.client.get('/signup')
+        # # go to signup page
+        # self.client.get('/signup')
 
-#     def tearDown(self):
-#         """ Tear down after every test. """
+    def tearDown(self):
+        """ Tear down after every test. """
 
-#         db.session.close()
-#         db.drop_all()
+        db.session.close()
+        db.drop_all()
 
-#     def test_passwords_do_not_match(self):
-#         """ Test when a new user does not enter the same password twice. """
+    def test_successful_signup(self):
+        """ Test when a new user does not enter the same password twice. """
 
-#         email = 'bob@codes.com'
-#         username = 'BobCodes1'
-#         password1 = 'Python101'
-#         password2 = 'Python102'
+        email = 'bob@codes.com'
+        username = 'BobCodes1'
+        password1 = 'Python101'
+        password2 = 'Python102'
 
-#         data = {'email': email,
-#                 'username': username, 
-#                 'password1': password1,
-#                 'password2': password2}
+        data = {'email': email,
+                'username': username, 
+                'password1': password1,
+                'password2': password2}
 
-#         self.client.get('/signup')
-#         # To complete later
+        result = self.client.post('/signup', data=data, follow_redirects=True)
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'<h2>You are not alone.</h2>', result.data)
+        self.assertIn(b'BobCodes1 successfully signed up.', result.data)
 
+    def test_successful_login(self):
+        """ Test when a new user does not enter the same password twice. """
 
-#     def test_username_already_exists(self):
-#         """ Test when a new user enters a username that is already taken. """
+        username = 'denisecodes'
+        password = 'Python'
 
-#         email = 'denise@codes.com'
-#         username = 'denisecodes'
-#         password1 = 'Python'
-#         password2 = 'Python'
+        data = {'username': username, 
+                'password': password}
 
-#         data = {'email': email,
-#                 'username': username, 
-#                 'password1': password1,
-#                 'password2': password2}
+        result = self.client.post('/login', data=data, follow_redirects=True)
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'<h2>You are not alone.</h2>', result.data)
+        self.assertIn(b'denisecodes successfully logged in.', result.data)
 
-#         self.client.get('/signup')
-#         # To complete later
+    def test_user_exists_found(self):
 
-#     def test_successful_signup(self):
+        username = 'denisecodes'
 
-#         email = 'bob@codes.com'
-#         username = 'BobCodes1'
-#         password1 = 'Python101'
-#         password2 = 'Python101'
+        data = {'username': username}
 
-#         data = {'email': email,
-#                 'username': username, 
-#                 'password1': password1,
-#                 'password2': password2}
+        result = self.client.get('/user_exists', data=data, follow_redirects=True)
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'found', result.data)
 
-#         self.client.get('/signup')
-#         # To complete later
+    def test_user_exists_not_found(self):
 
+        username = 'fabiocodes'
+
+        data = {'username': username}
+
+        result = self.client.get('/user_exists', data=data, follow_redirects=True)
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'not found', result.data)
+
+    def test_validate_login_valid(self):
+        
+        username = 'denisecodes'
+        password = 'Python'
+
+        data = {'username': username, 'password': password}
+
+        result = self.client.post('/validate_login', data=data, follow_redirects=True)
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'valid', result.data)
+
+    def test_validate_login_not_valid(self):
+        
+        username = 'fabiocodes'
+        password = 'Python'
+
+        data = {'username': username, 'password': password}
+
+        result = self.client.post('/validate_login', data=data, follow_redirects=True)
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'not valid', result.data)
 
 if __name__ == '__main__':
     unittest.main()
