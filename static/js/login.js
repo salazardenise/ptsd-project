d5176abbba12c5081cc16467a6e9d85c4c0cde9a
Denise Salazar <denise.salazar.1210@gmail.com>
2018-10-11 16:35:42 -0700

    Remove old function that was commented out

--- static/js/login.js
+++ static/js/login.js
@@ -26,28 +26,5 @@ $('#logInForm').on('submit', (evt) => {
             }
         }
     });
-
-    // $.get('/user_exists', {'username': username}, (result) => {
-    //     if (result != 'found') {
-    //         $('#logInErrorMessage').html("Error: Username not recognized.")
-    //         $('#logInErrorMessage').fadeIn(400, () => {
-    //             setTimeout(() => {$('#logInErrorMessage').fadeOut(400,)}, 5000);
-    //         });
-    //     } 
-    //     else {
-    //         $.post('/validate_login', 
-    //               {'username': username, 'password': password},
-    //               (result) => {
-    //                     if (result != 'valid') {
-    //                         $('#logInErrorMessage').html("Error: Username and password do not match")
-    //                         $('#logInErrorMessage').fadeIn(400, () => {
-    //                             setTimeout(() => {$('#logInErrorMessage').fadeOut(400,)}, 5000);
-    //                         });
-    //                     } else {
-    //                         // resubmit form
-    //                         $('#logInForm').unbind('submit').submit();
-    //                     }
-    //               });
-    //     }
-    // });
+    
 });
