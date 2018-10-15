a284fd658a98472132bbb3c59e09b708c372b187
Denise Salazar <denise.salazar.1210@gmail.com>
2018-10-15 12:52:40 -0700

    SET SQLALCHEMY_ECHO to True for researching queries for favoriting feature

--- model.py
+++ model.py
@@ -153,6 +153,7 @@ def connect_to_db(app, db_uri="postgresql:///ptsd"):
 
     # Configure to use a PostgreSQL database
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_ECHO'] = True
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
