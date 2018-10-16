02f22a4807fba9f5247f38ea6532f2671462580b
Denise Salazar <denise.salazar.1210@gmail.com>
2018-10-16 12:06:30 -0700

    Set SQLALCHEMY_ECHO to false

--- model.py
+++ model.py
@@ -153,7 +153,7 @@ def connect_to_db(app, db_uri="postgresql:///ptsd"):
 
     # Configure to use a PostgreSQL database
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_ECHO'] = False
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
