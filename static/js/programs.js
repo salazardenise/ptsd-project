0e9d65a56cd3e38ed59f80db449ec460d7554af9
Denise Salazar <denise.salazar.1210@gmail.com>
2018-10-18 16:31:39 -0700

    Attempt google map show program results

--- static/js/programs.js
+++ static/js/programs.js
@@ -9,17 +9,82 @@ $('#programSearchForm').on('submit', (evt) => {
     let data = {'search_text': search_text, 'search_type': search_type}
     $.get('/programs.json', data, (results) => {
         // results is a JSON object with all the programs that match the search
-
-        $('#programResults').empty(); 
         
         if (results.length == 0) {
-            $('#programResults').html("No results found. Try again with a different search."); 
+            $('#programsResults').empty()
+            $('#programsResults').html("No results found. Try again with a different search."); 
+            $('#programsMap').hide()
         } 
-        else {       
+        else { 
+            /////////////////////////////////////////////////////////
+            // PROCESS PROGRAMS RESULTS TO GET SET OF programs with addresses attached
+
+
+
+            // /////////////////////////////////////////////////////////
+            // // DISPLAY PROGRAMS RESULTS LOCATIONS IN MAP
+            // $('#programsMap').show()
+
+            // // create map
+            // let defaultLocationUS= {lat: 39, lng: -95};
+            // let map = new google.maps.Map(document.querySelector('#programsMap'), {
+            //     center: defaultLocationUS,
+            //     zoom: 6});
+
+            // // determine where to center map
+            // function setCenterOfMap(search_text) {
+            //     let location = new google.maps.Geocoder();
+
+            //     location.geocode({'address': search_text}, 
+            //         function (results, status) {
+            //             console.log(status);
+            //             if (status === google.maps.GeocoderStatus.OK) {
+            //                 console.log(status);
+            //                 map.setCenter(results[0].geometry.location);
+            //             } else {
+            //                 console.log('Geocode was not successful for the following reason: ' + status);
+            //             }
+            //         }
+            //     )
+            // }
+            // setCenterOfMap(search_text);
+
+            // // add each program to map with marker and info window
+            // function addProgramLocationByAddress(programName, programFullAddress) {
+            //     let programLocation = new google.maps.Geocoder();
+            //     let address = programFullAddress;
+            //     programLocation.geocode(
+            //         {'address': address, 'region': 'US'},
+            //         function(results, status) {
+            //             console.log(status);
+            //             if (status == google.maps.GeocoderStatus.OK) {
+            //                 let programLatLng = results[0].geometry.location;
+            //                 let programMarker = new google.maps.Marker({
+            //                     position: programLatLng,
+            //                     map: map,
+            //                     title: programName,
+            //                 }); 
+            //             } else {
+            //                 console.log('Geocode not successful for ' + address)
+            //             }
+            //         }
+            //     )
+            // }
+
+            // for (let i in results) {
+            //     let programFullAddress = results[i].address + ', ' + 
+            //                          results[i].city + ', ' + 
+            //                          results[i].state;
+            //     addProgramLocationByAddress(results[i].program_name, programFullAddress);
+            // }
+
+            ///////////////////////////////////////////////////////////
+            // DISPLAY PROGRAMS RESULTS IN TABLE      
             // add table headers
-            $('#programResults').append("<table>");
+            $('#programsResults').empty()
+            $('#programsResults').append("<table>");
             let table_heading = "<tr><th>Favorite</th><th>Program</th><th>Address</th><th>City</th><th>State</th><th>Zipcode</th></tr>";
-            $('#programResults').append(table_heading);
+            $('#programsResults').append(table_heading);
 
             // add each row for the table
             for (let i in results) {
@@ -42,9 +107,9 @@ $('#programSearchForm').on('submit', (evt) => {
                 let zipcode = results[i].zipcode;
                 row += `<td>${zipcode}</td>`;
                 row += '</tr>';
-                $('#programResults').append(row);
+                $('#programsResults').append(row);
             }
-            $('#programResults').append("</table>");
+            $('#programsResults').append("</table>");
         }
         
     });  
